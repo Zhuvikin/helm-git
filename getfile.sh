254c2db6bcb3cf00b03af9186f802c59a2a76cc7
Zhuvikin <zhuvikin@ya.ru>
2018-04-24 01:30:13 +0300

    Add HTTPS authentication support

--- getfile.sh
+++ getfile.sh
@@ -2,11 +2,14 @@
 
 set -e
 
-URI=$@ # eg: gitlab://username/project:master/kubernetes/helm-chart
+URI=$@ # eg: gitlab://username:password@account/project:master/kubernetes/helm-chart
 PROVIDER=$(echo $URI | cut -d: -f1) # eg: gitlab
-REPO=$(echo $URI | cut -d: -f2 | sed -e "s/\/\///") # eg: username/project
-BRANCH=$(echo $URI | cut -d: -f3 | cut -d/ -f1) # eg: master
-FILEPATH=$(echo $URI | cut -d: -f3 | sed -e "s/$BRANCH\///") # eg: kubernetes/helm-chart
+
+USERNAME=$(echo $URI | cut -d: -f2 | sed -e "s/\/\///") # eg: username
+PASSWORD=$(echo $URI | cut -d: -f3 | cut -d@ -f1) # eg: password
+REPO=$(echo $URI | cut -d@ -f2 | cut -d: -f1) # eg: account/project
+BRANCH=$(echo $URI | cut -d: -f4 | cut -d/ -f1) # eg: master
+FILEPATH=$(echo $URI | cut -d: -f4 | sed -e "s/$BRANCH\///") # eg: kubernetes/helm-chart
 
 # echo $URI $REPO $BRANCH $FILEPATH >&2
 
@@ -15,7 +18,7 @@ TMPDIR="$(mktemp -d)"
 cd $TMPDIR
 
 git init --quiet
-git remote add origin git@$PROVIDER.com:$REPO.git
+git remote add origin https://$USERNAME:$PASSWORD@$PROVIDER.com/$REPO.git
 git pull --depth=1 --quiet origin $BRANCH
 
 if [ -f $FILEPATH ]; then # if a file named $FILEPATH exists
