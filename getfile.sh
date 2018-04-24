4febeec2ae4d30d9968e1cf5ebc7d5f446a60d1f
Zhuvikin <zhuvikin@ya.ru>
2018-04-24 12:01:31 +0300

    Support branches with slash

--- getfile.sh
+++ getfile.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-URI=$@ # eg: gitlab://username:password@account/project:master/kubernetes/helm-chart
+URI=$@ # eg: gitlab://username:password@account/project:master:kubernetes/helm-chart
 PROVIDER=$(echo $URI | cut -d: -f1) # eg: gitlab
 
 USERNAME=$(echo $URI | cut -d: -f2 | sed -e "s/\/\///") # eg: username
 PASSWORD=$(echo $URI | cut -d: -f3 | cut -d@ -f1) # eg: password
 REPO=$(echo $URI | cut -d@ -f2 | cut -d: -f1) # eg: account/project
 BRANCH=$(echo $URI | cut -d: -f4 | cut -d/ -f1) # eg: master
-FILEPATH=$(echo $URI | cut -d: -f4 | sed -e "s/$BRANCH\///") # eg: kubernetes/helm-chart
+FILEPATH=$(echo $URI | cut -d: -f5) # eg: kubernetes/helm-chart
 
 # echo $URI $REPO $BRANCH $FILEPATH >&2
 
